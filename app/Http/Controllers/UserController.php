f58c1e02d7ea2e5c6390980331aee75c48f946fc
masriant <info.masrianto@gmail.com>
2020-10-13 14:35:48 +0700

    Http Request

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,24 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    public function index()
+    // public function index()
+    // {
+    //     $name = 'Masrianto';
+    //     $users = array(
+    //         "name" => "Lakip Center",
+    //         "email" => "info@lakipcenter.com",
+    //         "phone" => "085315448868"
+    //     );
+    //     return view('user', compact('name', 'users'));
+    // }
+
+    public function index(Request $request)
     {
-        $name = 'Masrianto';
-        $users = array(
-            "name" => "Lakip Center",
-            "email" => "info@lakipcenter.com",
-            "phone" => "085315448868"
-        );
-        return view('user', compact('name', 'users'));
+        // return $request->method();
+        // return $request->path();
+        // return $request->url();
+        return $request->fullUrl(); // http://127.0.0.1:8000/user?name=smith&age=34
+
+
     }
 }
